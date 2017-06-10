be04d3303c0ce2b41c4c8c1dc2075c107a9cccec
wh_Coder <461891475@qq.com>
2017-06-10 15:51:55 +0800

    scroll

--- androidApp/App.js
+++ androidApp/App.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react'
 import {
   View,
   Text,
+  ScrollView,
   StyleSheet,
+  RefreshControl,
   Button,
   TouchableOpacity,
   AppRegistry
@@ -18,20 +20,20 @@ import ScrollableTabView, {DefaultTabBar,} from 'react-native-scrollable-tab-vie
 
 class Webview extends Component {
   static navigationOptions = ({navigation}) => ({
-      headerTitle: (
-        <Text style={styles.headerTitle}>测试2</Text>
-      ),
-      headerStyle: {backgroundColor: theme.hpRed},
-      headerLeft: (
-        <TouchableOpacity style={styles.headerBtn} onPress={() => navigation.goBack()}>
-          <Icon name="ios-arrow-back" size={30} color="#fff" />
-        </TouchableOpacity>
-      ),
-      headerRight: (
-        <View style={styles.headerBtn}>
-          <Icon name="ios-more" size={30} color="#fff"/>
-        </View>
-      )
+    headerTitle: (
+      <Text style={styles.headerTitle}>测试2</Text>
+    ),
+    headerStyle: {backgroundColor: theme.hpRed},
+    headerLeft: (
+      <TouchableOpacity style={styles.headerBtn} onPress={() => navigation.goBack()}>
+        <Icon name="ios-arrow-back" size={30} color="#fff"/>
+      </TouchableOpacity>
+    ),
+    headerRight: (
+      <View style={styles.headerBtn}>
+        <Icon name="ios-more" size={30} color="#fff"/>
+      </View>
+    )
   });
 
   render() {
@@ -50,6 +52,18 @@ class Webview extends Component {
 
 class Root extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      arr: Array.from({length: 100}, (v, i) => i),
+      isRefreshing: false,
+    }
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+  }
+
   static navigationOptions = ({navigation}) => {
     return {
       headerTitle: (
@@ -67,8 +81,13 @@ class Root extends Component {
     };
   };
 
+  _onScroll() {
+    // console.log('滚动的回调')
+  }
+
   render() {
     const {navigate} = this.props.navigation;
+
     return (
       <View style={styles.container}>
         <View style={styles.content}>
@@ -84,12 +103,41 @@ class Root extends Component {
                 onPress={navigate.bind(this, 'Webview')}
               />
             </View>
-            <View tabLabel='栏目2'>
-              <Text>栏目1</Text>
-              <Button
-                title={'跳转2'}
-                onPress={navigate.bind(this, 'Webview')}
-              />
+            <View tabLabel='栏目2' style={{flex: 1}}>
+              <View style={styles.scrollBox}>
+                <ScrollView
+                  stickyHeaderIndices={[10]}
+                  ioszoomScale={0.5}
+                  ref={(scrollView) => {
+                    _scrollView = scrollView;
+                  }}
+                  style={{transform: [{rotate: '0deg'}]}}
+                  contentContainerStyle={{paddingVertical: 20}}
+                  horizontal={false}
+                  onScroll={this._onScroll.bind(this)}
+                  showsVerticalScrollIndicator={false}
+                  refreshControl={
+                    <RefreshControl
+                      refreshing={this.state.isRefreshing}
+                    />}
+                >
+                  {
+                    this.state.arr.map(function (value, index) {
+                      return (<Text key={index} style={{
+                        backgroundColor: 'pink',
+                        borderWidth: 1,
+                        borderColor: '#ccc'
+                      }}>{value}</Text>)
+                    })
+                  }
+                  <Button
+                    title={'置顶'}
+                    onPress={() => {
+                      _scrollView.scrollTo({y: 0, animated: true});
+                    }}
+                  />
+                </ScrollView>
+              </View>
             </View>
           </ScrollableTabView>
         </View>
@@ -152,14 +200,23 @@ const styles = StyleSheet.create({
   },
   tabText: {
     fontSize: 10
-  }
+  },
+  scrollBox: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+
 })
 
-const androidApp = StackNavigator({
+const RouteConfigs = {
   Root: {screen: Root},
   Webview: {screen: Webview}
-},{
-  mode: 'modal'
-});
+}
+
+const StackNavigatorConfig = {
+  //
+}
+
+const androidApp = StackNavigator(RouteConfigs, StackNavigatorConfig);
 
 AppRegistry.registerComponent('androidApp', () => androidApp);
